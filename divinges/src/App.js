3c7cb3a97cf6dd56a484cc3560f15cdeb6e3380e
Mosorior <mosorior173@gmail.com>
2024-01-15 13:14:10 +0100

    Tierra girando

--- divinges/src/App.js
+++ divinges/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Canvas camera={{ position: [0, 0, 5] }}>
-        <ambientLight intensity={0.5} />
+        <ambientLight intensity={2} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <Earth />
       </Canvas>
--- divinges/src/components/Earth.js
+++ divinges/src/components/Earth.js
@@ -1,20 +1,51 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
-import { Sphere } from '@react-three/drei';
+import { Sphere, useTexture } from '@react-three/drei';
+import earthTexture from "./8081_earth.jpg";
 
-const Earth = () => {
+export const Earth = () => {
   const earthRef = useRef();
+  const texture = useTexture(earthTexture);
+  const [isDragging, setDragging] = useState(false);
 
-  // Rotar la Tierra usando useFrame
+  const handlePointerDown = () => {
+    setDragging(true);
+  };
+
+  const handlePointerUp = () => {
+    setDragging(false);
+  };
+
+  const handlePointerMove = (event) => {
+    if (isDragging) {
+      const { movementX, movementY } = event.nativeEvent;
+      earthRef.current.rotation.y += movementX / 300;
+      earthRef.current.rotation.x += movementY / 300;
+    }
+  };
+
+  const handlePointerLeave = () => {
+    setDragging(false);
+  };
+
+
+  // Rotar la Tierra automáticamente
   useFrame(() => {
-    if (earthRef.current) {
-      earthRef.current.rotation.y += 0.005;
+    if (!isDragging && earthRef.current) {
+      earthRef.current.rotation.y -= 0.0005;
     }
   });
 
   return (
-    <Sphere ref={earthRef} args={[2, 64, 64]}>
-      <meshStandardMaterial attach="material" color="blue" />
+    <Sphere
+      ref={earthRef}
+      args={[2, 64, 64]}
+      onPointerDown={handlePointerDown}
+      onPointerUp={handlePointerUp}
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+    >
+      <meshStandardMaterial attach="material" map={texture} />
     </Sphere>
   );
 };
