97cacb2a63bffdabe71ac99d25db9bcb230add2f
Mosorior <mosorior173@gmail.com>
2024-01-16 11:44:01 +0100

    Revert "Texto Logo Animado"
    This reverts commit de349015b7c529b3b56de2ee8a67a55b150af406.

--- divinges/src/App.js
+++ divinges/src/App.js
@@ -1,27 +1,19 @@
-import './style/App.css';
+import './style/App.css'
+import './style/Logo.css'
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
 import Earth from './components/Earth';
+import Logo from './components/Logo';
 
 const App = () => {
-
-  
-
   return (
-    <div className="app-container">
+    <div style={{ backgroundColor: '#000', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Canvas camera={{ position: [0, 0, 2] }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Earth />
-        <OrbitControls enablePan={false} enableZoom={false} enableDamping dampingFactor={0.25} rotateSpeed={0.3} />
+        
       </Canvas>
-      <div className="overlay">
-        <div className="text">
-          <span className="small">Diving</span>
-          <span className="large">ES</span>
-        </div>
-      </div>
     </div>
   );
 };
