397d4c8dff8b30f80259c0c26e0a4ba31761f63c
Mosorior <mosorior173@gmail.com>
2024-01-16 14:06:49 +0100

    Earth.js Limpio
    Earth.js libre de código innecesario o no usado aún.

--- divinges/src/components/Earth.js
+++ divinges/src/components/Earth.js
@@ -6,8 +6,6 @@ import { useNavigate } from 'react-router-dom';
 
 const Earth = ({ setShowText }) => {
   const earthRef = useRef();
-  const navigate = useNavigate();
-  const [zooming, setZooming] = useState(false);
 
   const { scene } = useGLTF('/models/RealEarth.glb');
 
@@ -16,31 +14,11 @@ const Earth = ({ setShowText }) => {
       // Rotación automática
       earthRef.current.rotation.x += 0.001;
       earthRef.current.rotation.y -= 0.005;
-
-      // Zoom localizado hacia España
-      if (zooming) {
-        const targetPosition = [0, 0, 1.5]; // Cambiar la posición según tus necesidades
-        earthRef.current.position.lerp(targetPosition, 0.05);
       }
-    }
-  });
-
-  const handleClick = async () => {
-    // Ocultar las letras antes de redirigir
-    setShowText(false);
-
-    // Iniciar zoom
-    setZooming(true);
-
-    // Esperar 3 segundos (ajustar según tus necesidades)
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    // Redirigir después de la animación de zoom
-    navigate('/home');
-  };
+    });
 
   return (
-    <group onClick={handleClick}>
+    <group>
       <primitive ref={earthRef} object={scene} />
     </group>
   );
