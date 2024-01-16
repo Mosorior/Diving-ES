2484c624d6a3d79334a8b2949090329ecd32eca6
Mosorior <mosorior173@gmail.com>
2024-01-16 10:15:49 +0100

    Modelo 3D insertado en la web
    Modelo 3D holográfico de la Tierra insertado

--- divinges/src/App.js
+++ divinges/src/App.js
@@ -1,14 +1,16 @@
-
+import './style/App.css'
+import './style/Logo.css'
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import Earth from './components/Earth';
+import Logo from './components/Logo';
 
 const App = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <Canvas camera={{ position: [0, 0, 5] }}>
-        <ambientLight intensity={2} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
         <Earth />
       </Canvas>
     </div>
--- divinges/src/components/Earth.js
+++ divinges/src/components/Earth.js
@@ -1,53 +1,19 @@
-import React, { useRef, useState } from 'react';
-import { useFrame } from '@react-three/fiber';
-import { Sphere, useTexture } from '@react-three/drei';
-import earthTexture from "./8081_earth.jpg";
+import React, { useRef } from 'react';
+import { useFrame, useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
 
-export const Earth = () => {
+const Earth = () => {
   const earthRef = useRef();
-  const texture = useTexture(earthTexture);
-  const [isDragging, setDragging] = useState(false);
+  const { scene } = useLoader(GLTFLoader, '/models/RealEarth.glb');
 
-  const handlePointerDown = () => {
-    setDragging(true);
-  };
-
-  const handlePointerUp = () => {
-    setDragging(false);
-  };
-
-  const handlePointerMove = (event) => {
-    if (isDragging) {
-      const { movementX, movementY } = event.nativeEvent;
-      earthRef.current.rotation.y += movementX / 300;
-      earthRef.current.rotation.x += movementY / 300;
-    }
-  };
-
-  const handlePointerLeave = () => {
-    setDragging(false);
-  };
-
-
-  // Rotar la Tierra automáticamente
   useFrame(() => {
-    if (!isDragging && earthRef.current) {
-      earthRef.current.rotation.y -= 0.0005;
+    if (earthRef.current) {
+      earthRef.current.rotation.x += 0.01;
+      earthRef.current.rotation.y += 0.01;
     }
   });
 
-  return (
-    <Sphere
-      ref={earthRef}
-      args={[2, 64, 64]}
-      onPointerDown={handlePointerDown}
-      onPointerUp={handlePointerUp}
-      onPointerMove={handlePointerMove}
-      onPointerLeave={handlePointerLeave}
-    >
-      <meshStandardMaterial attach="material" map={texture} />
-    </Sphere>
-  );
+  return <primitive ref={earthRef} object={scene} />;
 };
 
 export default Earth;
