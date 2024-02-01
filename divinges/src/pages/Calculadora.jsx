7169bc74e317c2d0067c5bed8ad9e4e3ef43838d
Mosorior <mosorior173@gmail.com>
2024-02-02 00:01:15 +0100

    CalculadoraCSS Casi completo

--- divinges/src/pages/Calculadora.jsx
+++ divinges/src/pages/Calculadora.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 import Navbar from '../components/Navigation/Navbar';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../style/Calculadora.css';
 
 const Calculadora = () => {
-    const [profundidad, setProfundidad] = useState(0);
-    const [tiempoFondo, setTiempoFondo] = useState(0);
-    const [presion, setPresion] = useState(0);
-    const [planBuceo, setPlanBuceo] = useState('');
-    const [mostrarDisclaimer, setMostrarDisclaimer] = useState(true); // Estado para controlar la visualización del modal
+    const [profundidad, setProfundidad] = useState('');
+    const [tiempoFondo, setTiempoFondo] = useState('');
+    const [presion, setPresion] = useState('');
+    const [planDeBuceo, setPlanDeBuceo] = useState({
+        tiempoDescenso: '',
+        tiempoFondo: '',
+        tiempoAscenso: '',
+        requiereDescompresion: false,
+        tiempoDescompresion: '',
+        profundidadDescompresion: '',
+    });
+    const [mostrarDisclaimer, setMostrarDisclaimer] = useState(true);
+    const [mostrarInfoImportante, setMostrarInfoImportante] = useState(false);
+
+
+    const toggleMostrarInfo = () => {
+        setMostrarInfoImportante(!mostrarInfoImportante);
+    };
 
     const calcularTiempoParadaDescompresion = (profundidad, tiempoFondo) => {
         const tablaND = {
@@ -27,34 +41,32 @@ const Calculadora = () => {
     };
 
     const calcularPresionYBuceo = () => {
-        if (profundidad > 45 || tiempoFondo > 200 || profundidad < 12) {
-            setPlanBuceo('Los valores ingresados no son realistas. Por favor, ajusta la profundidad y/o el tiempo en el fondo.');
+        if (profundidad > 45 || tiempoFondo > 200) {
+            setPlanDeBuceo(prevState => ({ ...prevState, mensaje: 'Los valores ingresados no son realistas. Por favor, ajusta la profundidad y/o el tiempo en el fondo.' }));
             return;
         }
 
-        // Cálculo del tiempo de descenso y ascenso
         const velocidadDescenso = 18;
         const velocidadAscenso = 9;
         const tiempoDescenso = profundidad / velocidadDescenso;
         const tiempoAscenso = profundidad / velocidadAscenso;
 
-        // Cálculo de la presión
         const rho = 1025;
         const g = 9.81;
         const Patm = 1;
         const P = Patm + (rho * g * profundidad) / 101325;
         setPresion(P.toFixed(2));
 
-        // Cálculo de la parada de descompresión
         const { requiereDescompresion, tiempoDescompresion, profundidadDescompresion } = calcularTiempoParadaDescompresion(profundidad, tiempoFondo);
 
-        // Plan de buceo
-        setPlanBuceo(`
-            Tiempo de descenso: ${tiempoDescenso.toFixed(2)} minutos
-            Tiempo en el fondo: ${tiempoFondo.toFixed(2)} minutos
-            Tiempo de ascenso: ${tiempoAscenso.toFixed(2)} minutos
-            ${requiereDescompresion ? `Tiempo de parada de descompresión: ${tiempoDescompresion.toFixed(2)} minutos a una profundidad de ${profundidadDescompresion} metros` : 'No se requiere parada de descompresión.'}
-        `);
+        setPlanDeBuceo({
+            tiempoDescenso: convertirTiempo(tiempoDescenso),
+            tiempoFondo: convertirTiempo(tiempoFondo),
+            tiempoAscenso: convertirTiempo(tiempoAscenso),
+            requiereDescompresion: requiereDescompresion,
+            tiempoDescompresion: convertirTiempo(tiempoDescompresion),
+            profundidadDescompresion: profundidadDescompresion,
+        });
     };
 
     const aceptarDisclaimer = () => {
@@ -62,65 +74,85 @@ const Calculadora = () => {
     };
 
     const rechazarDisclaimer = () => {
-        window.location.href = '/'; // Redirige a la página de inicio
+        window.location.href = '/home';
     };
+
+    const convertirTiempo = (tiempoDecimal) => {
+        const minutos = Math.floor(tiempoDecimal);
+        const segundos = Math.round((tiempoDecimal - minutos) * 60);
+        return `${minutos}min${segundos !== 0 ? ` ${segundos}s` : ''}`;
+      };
+      
 
     return (
         <div>
             <Navbar />
-            <h2>Calculadora de Buceo</h2>
-
-            {mostrarDisclaimer && (
-                <div className="modal">
-                    <div className="modal-content">
-                    <h3>Descargo de Responsabilidad</h3>
+            <div className="calculadora-container">
+                <h2>Calculadora de Buceo</h2>
+                <div className='info-important' onClick={() => setMostrarInfoImportante(true)} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+                    <i className="fas fa-exclamation-triangle icono-advertencia"></i>
+                    <span>Información Importante</span>
+                </div>
+                {mostrarInfoImportante && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <h3>Información Importante</h3>
+                            <p>Los cálculos proporcionados por esta herramienta son aproximados y se basan en los siguientes valores y suposiciones:</p>
+                            <div className="info-grid">
+                                <div>
+                                    <p>Velocidad de descenso: 18 metros por minuto.</p>
+                                    <p>Velocidad de ascenso: 9 metros por minuto.</p>
+                                    <p>Tabla de No Descompresión (ND) para calcular la necesidad de paradas de descompresión.</p>
+                                </div>
+                                <div>
+                                    <p>Tiempo de parada de descompresión estimado: 5 minutos a una profundidad de 5 metros (cuando se requiere).</p>
+                                    <p>Los cálculos de presión asumen una densidad del agua de mar de 1025 kg/m³ y una aceleración debido a la gravedad de 9.81 m/s².</p>
+                                </div>
+                            </div>
+                            <p>Además, se recomienda planificar el consumo de aire/gas de manera conservadora y llevar siempre una reserva adicional de aire/gas más allá del cálculo realizado.</p>
+                            <button onClick={() => setMostrarInfoImportante(false)}>Cerrar</button>
+                        </div>
+                    </div>
+                )}
+            
+                {mostrarDisclaimer && (
+                    <div className="modal">
+                        <div className="modal-content">
+                        <h3>Descargo de Responsabilidad</h3>
                     <p>Esta herramienta de cálculo de buceo se proporciona "tal cual" y es solo para fines informativos. Los resultados son estimaciones y pueden no ser precisos para todas las situaciones de buceo. El creador de esta herramienta no garantiza la exactitud, adecuación o integridad de la información y materiales proporcionados y expresamente se exime de responsabilidad por errores u omisiones en estos materiales.</p>
                 <p>El uso de esta herramienta no reemplaza la capacitación profesional de buceo, el asesoramiento de expertos, o el uso de un ordenador de buceo certificado. Los usuarios son responsables de su propia seguridad y deben tomar todas las precauciones necesarias al planificar y realizar inmersiones. El creador de esta herramienta no será responsable de ninguna lesión, pérdida, daño o accidente que pueda resultar del uso o confianza en la información proporcionada por esta herramienta.</p>
                 <p>Al utilizar esta herramienta, usted reconoce y acepta que el buceo conlleva riesgos inherentes y que es responsable de tomar decisiones informadas y seguras en relación con sus actividades de buceo.</p>
-                    <button onClick={aceptarDisclaimer}>Aceptar</button>
-                    <button onClick={rechazarDisclaimer}>Rechazar</button>
+                    
+                            <button onClick={aceptarDisclaimer}>Aceptar</button>
+                            <button onClick={rechazarDisclaimer}>Rechazar</button>
+                        </div>
+                    </div>
+                )}
+                <div>
+                    <label htmlFor="profundidad">Profundidad (metros):</label>
+                    <input type="number" id="profundidad" value={profundidad} onChange={(e) => setProfundidad(Number(e.target.value))} placeholder="Ej. 30"/>
                 </div>
+                <div>
+                    <label htmlFor="tiempoFondo">Tiempo en el fondo (minutos):</label>
+                    <input type="number" id="tiempoFondo" value={tiempoFondo} onChange={(e) => setTiempoFondo(Number(e.target.value))} placeholder="Ej. 20"/>
                 </div>
-            )}
-            <div>
-                <label htmlFor="profundidad">Profundidad (metros):</label>
-                <input
-                    type="number"
-                    id="profundidad"
-                    value={profundidad}
-                    onChange={(e) => setProfundidad(Number(e.target.value))}
-                    placeholder="Ej. 30"
-                />
-            </div>
-            <div>
-                <label htmlFor="tiempoFondo">Tiempo en el fondo (minutos):</label>
-                <input
-                    type="number"
-                    id="tiempoFondo"
-                    value={tiempoFondo}
-                    onChange={(e) => setTiempoFondo(Number(e.target.value))}
-                    placeholder="Ej. 20"
-                />
-            </div>
-            <button onClick={calcularPresionYBuceo}>Calcular Planificación</button>
-            {presion && <p>Presión Requerida: {presion} atm</p>}
-            {planBuceo && <div><h3>Plan de Buceo:</h3><p>{planBuceo}</p></div>}
-
-            <div className="info-seccion">
-                <h3>Información Importante</h3>
-                <p>Los cálculos proporcionados por esta herramienta son aproximados y se basan en los siguientes valores y suposiciones:</p>
-                <ul>
-                    <li>Velocidad de descenso: 18 metros por minuto.</li>
-                    <li>Velocidad de ascenso: 9 metros por minuto.</li>
-                    <li>Tabla de No Descompresión (ND) para calcular la necesidad de paradas de descompresión.</li>
-                    <li>Tiempo de parada de descompresión estimado: 5 minutos a una profundidad de 5 metros (cuando se requiere).</li>
-                    <li>Los cálculos de presión asumen una densidad del agua de mar de 1025 kg/m³ y una aceleración debido a la gravedad de 9.81 m/s².</li>
-                </ul>
-                 <p>Además, se recomienda planificar el consumo de aire/gas de manera conservadora y llevar siempre una reserva adicional de aire/gas más allá del cálculo realizado. Las condiciones bajo el agua pueden cambiar y es posible que se consuma más aire de lo previsto. Siempre es mejor tener una cantidad de gas en reserva para emergencias.</p>
-                <p>Es importante recordar que estos cálculos no deben usarse para la planificación real de inmersiones. Siempre consulte a un profesional y utilice un ordenador de buceo para la planificación detallada de sus inmersiones.</p>
+                <button onClick={calcularPresionYBuceo}>Calcular Planificación</button>
+                {presion && <p>Presión Requerida: <b>{presion} atm</b></p>}
+                {planDeBuceo.tiempoDescenso && (
+                    <div className='resultados'>
+                        <h3>Plan de Buceo:</h3>
+                        <p>Tiempo de descenso: {planDeBuceo.tiempoDescenso}</p>
+                        <p>Tiempo en el fondo: {planDeBuceo.tiempoFondo}</p>
+                        <p>Tiempo de ascenso: {planDeBuceo.tiempoAscenso}</p>
+                        {planDeBuceo.requiereDescompresion && (
+                            <p>Tiempo de parada de descompresión: {planDeBuceo.tiempoDescompresion} a una profundidad de {planDeBuceo.profundidadDescompresion} metros</p>
+                        )}
+                        {!planDeBuceo.requiereDescompresion && <p>No se requiere parada de descompresión.</p>}
+                    </div>
+                )}
             </div>
         </div>
     );
 };
 
-export default Calculadora;
+export default Calculadora;            
